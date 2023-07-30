50608ec7ef5539115287b8a511af37b6cb032886
NilsHeil <56686609+NilsHeil@users.noreply.github.com>
2023-07-30 17:21:37 -0600

    pubmed

--- train_roberta.py
+++ train_roberta.py
@@ -140,7 +140,7 @@ sweep_config['metric'] = metric
 #sweep_id = wandb.sweep(sweep_config, project="helpfulness")
 
 
-dataset='translation'
+dataset='pubmed'
 #datasets=['PAWS','translation','pubmed','logic','django','spider']
 ## True for balancing the observations in the loss function (currently not working)
 compute_weights=False
@@ -151,7 +151,7 @@ decision_metric='eval_'+d_metric
 outcome_variable='helpfulness'
 ## final results files
 ##https://drive.google.com/drive/folders/17pwdiiu7U1oyly8YwMtqCRdu3GBIWT3K
-file_path='final_results_trans_corrected.csv'
+file_path='final_results_pubmed_corrected.csv'
 logs_path=''
 run_name=dataset+"_hyp_final_"+outcome_variable
 
