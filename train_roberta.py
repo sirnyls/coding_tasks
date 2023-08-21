26568069c9b01655d82ace8a5a439134fd395f38
sirnyls <nils.heil@web.de>
2023-08-21 21:39:11 +0200

    logic dataset

--- train_roberta.py
+++ train_roberta.py
@@ -121,7 +121,7 @@ sweep_config['metric'] = metric
 #sweep_id = wandb.sweep(sweep_config, project="helpfulness")
 
 
-dataset='translation'
+dataset='logic'
 #datasets=['PAWS','translation','pubmed','logic','django','spider']
 ## True for balancing the observations in the loss function (currently not working)
 compute_weights=False
@@ -132,7 +132,7 @@ decision_metric='eval_'+d_metric
 outcome_variable='helpfulness'
 ## final results files
 ##https://drive.google.com/drive/folders/17pwdiiu7U1oyly8YwMtqCRdu3GBIWT3K
-file_path='final_results_trans_corrected.csv'
+file_path='final_results_logic_corrected.csv'
 logs_path=''
 run_name=dataset+"_hyp_final_"+outcome_variable
 
