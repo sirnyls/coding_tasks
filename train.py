e9f0150d0922e4f293f34447c139832c4f063bfc
sirnyls <nils.heil@web.de>
2023-08-21 15:19:16 +0200

    e

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torch
 import numpy as np
 
@@ -16,6 +16,29 @@ data = data.loc[:, ['id', 'text', 'label']]
 data = data.loc[~data.text.isna()]
 
 train_data, val_data = train_test_split(data, test_size=0.1, stratify=data['label'])
+
+
+class CustomDataset(Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['label'] = self.labels[idx]
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+# Update the tokenize_data function
+def tokenize_data(data):
+    tokenized = tokenizer(data['text'].tolist(), padding=True, truncation=True, max_length=512, return_tensors='pt')
+    labels = torch.tensor(data['label'].tolist())
+    return CustomDataset(tokenized, labels)
+
+train_dataset = tokenize_data(train_data)
+val_dataset = tokenize_data(val_data)
 
 # 2. Tokenize the data
 tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
